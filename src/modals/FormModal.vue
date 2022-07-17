41787b719e55ec06ec5725cdba50dc4a853d48a1
Jon <jon.howell314@gmail.com>
2022-07-17 11:03:55 +0800

    created first modal page

---
+++ src/modals/FormModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>This is a form modal or something</div>
+  <button @click="onClose">Close</button>
+</template>
+
+<script setup>
+import { modalController } from '@ionic/vue';
+
+const onClose = () => {
+  modalController.dismiss();
+};
+</script>
--- src/views/BooksPage.vue
+++ src/views/BooksPage.vue
@@ -1,7 +1,21 @@
 <template>
-  <BasePage> Book page coming soon </BasePage>
+  <BasePage>
+    Book page coming soon
+
+    <button @click="onOpenModal">Open Modal</button>
+  </BasePage>
 </template>
 
 <script setup>
 import BasePage from './BasePage.vue';
+import FormModal from '@/modals/FormModal.vue';
+
+import { modalController } from '@ionic/vue';
+
+const onOpenModal = async () => {
+  const modal = await modalController.create({
+    component: FormModal,
+  });
+  return modal.present();
+};
 </script>
