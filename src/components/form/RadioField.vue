0baf7290a2917aa2820c1beb3d342c323136a4e8
Jon <jon.howell314@gmail.com>
2022-07-25 22:02:47 +0800

    Added radio field

---
+++ src/components/form/RadioField.vue
@@ -0,0 +1,39 @@
+<template>
+  <FormField class="radio-field" :name="props.name" :label="props.label">
+    <ion-radio-group :value="props.modelValue" v-bind="$attrs" @ion-change="onChange">
+      <slot></slot>
+    </ion-radio-group>
+  </FormField>
+</template>
+
+<script setup>
+import { IonRadioGroup } from '@ionic/vue';
+import { useField } from './form.js';
+import FormField from './FormField.vue';
+
+const props = defineProps({
+  modelValue: undefined,
+  name: { type: String, required: true },
+  label: String,
+});
+
+const emits = defineEmits(['update:modelValue']);
+
+const { validate } = useField(props.name);
+
+const onChange = e => {
+  console.dir(e.target.value);
+  emits('update:modelValue', e.target.value);
+  validate();
+};
+</script>
+
+<style>
+.radio-field .field-input {
+  border: 1px solid transparent;
+}
+
+.radio-field .field-input.error {
+  border-color: #dc3545;
+}
+</style>
